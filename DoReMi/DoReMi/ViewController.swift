4d05f37af596c8f1d60ea0b11457dafaa7fb3554
David Teo <dspirit@gmail.com>
2016-04-26 15:08:20 -0700

    Completed DoReMi Exercise

--- DoReMi/DoReMi/ViewController.swift
+++ DoReMi/DoReMi/ViewController.swift
@@ -19,16 +19,25 @@ class ViewController: UIViewController, UITableViewDataSource {
     
     let model = [
         // TODO: Fill this array with data
+        "Do",
+        "Re",
+        "Mi"
     ]
     
     // Add the two essential table data source methods here
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         //TODO: Implement method to return the correct number of rows.
+        return self.model.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         //TODO: Implement method to return cell with the correct reuseidentifier and populated with the correct data.
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellReuseIdentifier)!
+        let row = self.model[indexPath.row]
+        cell.textLabel?.text = row
+        
+        return cell
     }
 
 }
